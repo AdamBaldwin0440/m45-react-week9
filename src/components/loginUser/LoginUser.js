c336fce8454006d6bb9cc4566f090c775b5d7ab8
Adam Baldwin <adi.baldwin@hotmail.com>
2023-03-09 14:23:44 +0000

    add user register and login components

---
+++ src/components/loginUser/LoginUser.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./LoginUser.css";
+
+const LoginUser = () => {
+    return (
+        <div className="loginUser-wrapper"> 
+        <form className="login-user-form">
+            <label>
+                Username:
+                <input placeholder="username"/>
+            </label>
+            <label>
+                Password:
+                <input placeholder="password"/>
+            </label>
+            <button>Login</button>
+        </form>
+    </div>
+    )
+};
+    export default LoginUser
